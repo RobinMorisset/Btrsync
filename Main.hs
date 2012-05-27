5ed695679e0322bced6e26c6919166d0edc4f631
Florian Bourse <fbourse@clipper.ens.fr>
2012-05-27 18:13:57 +0200

    refactoring main - Work In Progress

--- Main.hs
+++ Main.hs
@@ -11,6 +11,7 @@ import System.Console.GetOpt
 import System.Environment
 import System.Exit
 import System.Random
+import Network
 
 import Config
 import Hashing
@@ -42,59 +43,114 @@ mainO input output inputEnd =
     
 
 main :: IO ()
-main = do
-    args <- getArgs
-    (config, t1, t2) <- parseArgs args
-    g <- case seed config of
-            Nothing -> getStdGen
-            Just s -> return $ mkStdGen s
-    let low = 2 ^ (pSize config) :: Integer
-        high = 2 ^ (pSize config + 1) :: Integer
+main = 
+    case role config of
+	Main -> do
+            args <- getArgs
+            (config, t1, t2) <- parseArgs args
+            g <- case seed config of
+                    Nothing -> getStdGen
+                    Just s -> return $ mkStdGen s
+            let low = 2 ^ (pSize config) :: Integer
+                high = 2 ^ (pSize config + 1) :: Integer
 
-    (_, files1, _) <- toDir $ dir t1
-    (_, files2, _) <- toDir $ dir t2
+            (_, files1, _) <- toDir $ dir t1
+            (_, files2, _) <- toDir $ dir t2
 
-    (newFiles, deleteFiles) <- (flip evalStateT) g $ do
-        filesPrime1 <- mapKeysM nextShiftedPrime files1
-        filesPrime2 <- mapKeysM nextShiftedPrime files2
-        let ks1 = M.keys filesPrime1
+            (newFiles, deleteFiles) <- (flip evalStateT) g $ do
+                filesPrime1 <- mapKeysM nextShiftedPrime files1
+                filesPrime2 <- mapKeysM nextShiftedPrime files2
+                let ks1 = M.keys filesPrime1
+                let ks2 = M.keys filesPrime2 
+
+                let whilenot :: Integer -> Integer -> StateT StdGen IO ([Hash], [Hash])
+                    whilenot oldD modulo = do
+                    p <- makePrime low high 
+                    let newP = p * modulo 
+
+                    -- Currently, we are only considering not-recursive dirs
+                    let pi1 = mkProduct p ks1
+                    let pi2 = mkProduct p ks2
+
+                    let d' = (pi1 * modularInv p pi2) `mod` p 
+                    let d = crt [(d',p), (oldD, modulo)]
+                    let (a, b) = minFraction d newP
+
+                    let newHashes = detChanges a ks1
+                        deleteHashes = detChanges b ks2
+                        okNew = (product newHashes `mod` newP) == a
+                        okDelete = (product deleteHashes `mod` newP) == b
+
+                    if (okNew && okDelete) 
+                        then return (newHashes, deleteHashes)
+                        else whilenot d newP
+                
+                (newHashes, deleteHashes) <- whilenot 0 1
+            
+                let newFiles = catMaybes $ 
+                        map (\h -> M.lookup h filesPrime1) newHashes
+                    deleteFiles = catMaybes $ 
+                        map (\h -> M.lookup h filesPrime2) deleteHashes
+                return (newFiles, deleteFiles)
+
+            print newFiles
+            print deleteFiles
+            exitSuccess
+
+    Oscar -> withSocketsDo $ do
+        args <- getArgs
+        (config, t1, t2) <- parseArgs args
+        channel <- connectTo 
+        oscarg <- getStdGen
+        maybeneil <- host t1
+        let hostname = case maybeneil of
+                    Nothing -> error "no hostname for neil."
+                    Just neil -> neil
+        (_, files2, _) <- toDir $ dir t2
+        channel <- connectTo hostname
+        (flip evalStateT) oscarg $ do
+            filesPrime2 <- mapKeysM nextShiftedPrime files2
         let ks2 = M.keys filesPrime2 
-
-        let whilenot :: Integer -> Integer -> StateT StdGen IO ([Hash], [Hash])
-            whilenot oldD modulo = do
-            p <- makePrime low high 
-            let newP = p * modulo 
-
-            -- Currently, we are only considering not-recursive dirs
-            let pi1 = mkProduct p ks1
+        g <- case seed config of
+                Nothing -> error "no seed sent by Main."
+                Just s -> return $ mkStdGen s
+        let low = 2 ^ (pSize config) :: Integer
+            high = 2 ^ (pSize config + 1) :: Integer
+        hSetBuffering channel LineBuffering        
+        let dowhile =
+            (flip evalStateT) g $ do
+                p <- makePrime low high 
             let pi2 = mkProduct p ks2
-
-            let d' = (pi1 * modularInv p pi2) `mod` p 
-            let d = crt [(d',p), (oldD, modulo)]
-            let (a, b) = minFraction d newP
-
-            let newHashes = detChanges a ks1
-                deleteHashes = detChanges b ks2
-                okNew = (product newHashes `mod` newP) == a
-                okDelete = (product deleteHashes `mod` newP) == b
-
-            if (okNew && okDelete) 
-                then return (newHashes, deleteHashes)
-                else whilenot d newP
+            hPutStrLn channel $ show pi2
+            let neilData = hGetLine channel
+            if neilData == ""
+                then dowhile
+                else 
+                    
+        dowhile
+ 
+    Neil -> withSocketsDo $ do 
+        args <- getArgs
+        (config, t1, t2) <- parseArgs args
+        socket <- listenOn port
+        (channel, _, _) <- accept socket
+        nielg <- getStdGen 
+        (_, files1, _) <- toDir $ dir t1
+        (flip evalStateT) nielg $ do
+            filesPrime1 <- mapKeysM nextShiftedPrime files1
+        let ks1 = M.keys filesPrime1
+        g <- case seed config of
+                Nothing -> error "no seed sent by Main."
+                Just s -> return $ mkStdGen s
+        let low = 2 ^ (pSize config) :: Integer
+            high = 2 ^ (pSize config + 1) :: Integer
+        hSetBuffering channel LineBuffering
+        p <- makePrime low high
         
-        (newHashes, deleteHashes) <- whilenot 0 1
-    
-        let newFiles = catMaybes $ 
-                map (\h -> M.lookup h filesPrime1) newHashes
-            deleteFiles = catMaybes $ 
-                map (\h -> M.lookup h filesPrime2) deleteHashes
-        return (newFiles, deleteFiles)
-
-    print newFiles
-    print deleteFiles
-    exitSuccess
-
+ 
+  
 -- | This contains all the computations on Neil side for a round
+
 roundN :: Integer -> Integer -> Integer -> Integer -> [Hash]
         -> (Integer, Integer, Maybe (Integer, [Hash]))
 roundN oldD oldPs p pi2 ks1 =
@@ -111,7 +167,7 @@ roundN oldD oldPs p pi2 ks1 =
         then (newPs, d, Just (b, newHashes))
         else (newPs, d, Nothing)
 
-roundO :: Integer -> [Hash] -> [Hash]
+roundO :: Integer -> [Hash] -> Integer -> [Hash]
 roundO b ks2 newPs =
     let deleteHashes = detChanges b ks2
     in deleteHashes
