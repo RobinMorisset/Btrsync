781fe53f75d2d375ae6333cdadfd56f09b7ec33d
Robin Morisset <robin.morisset@ens.fr>
2012-05-28 09:58:29 +0200

    small refactoring/cleaning of Main

--- Main.hs
+++ Main.hs
@@ -29,16 +29,6 @@ mapKeysM f =
             return (k', v)
         ) . M.toList
 
--- | makePrime low high returns p such that
--- p >= low
--- not exists p' s.t. p > p' >= high
-makePrime :: Integer -> Integer -> StateT StdGen IO Integer
-makePrime low high = do
-    g <- get
-    let (preP, g') = randomR (low, high) g
-    put g'
-    nextPrime preP
-
 port :: PortID
 port = undefined
 
@@ -46,56 +36,13 @@ main :: IO ()
 main = do 
     args <- getArgs
     (config, t1, t2) <- parseArgs args        
+    g <- case seed config of
+        Nothing -> error "no seed sent by Main."
+        Just s -> return $ mkStdGen s
+    let low = 2 ^ (pSize config) :: Integer
+        high = 2 ^ (pSize config + 1) :: Integer
     case role config of
-	Main -> do
-            g <- case seed config of
-                    Nothing -> getStdGen
-                    Just s -> return $ mkStdGen s
-            let low = 2 ^ (pSize config) :: Integer
-                high = 2 ^ (pSize config + 1) :: Integer
-
-            (_, files1, _) <- toDir (dir t1) ""
-            (_, files2, _) <- toDir (dir t2) ""
-
-            (newFiles, deleteFiles) <- (flip evalStateT) g $ do
-                filesPrime1 <- mapKeysM nextShiftedPrime files1
-                filesPrime2 <- mapKeysM nextShiftedPrime files2
-                let ks1 = M.keys filesPrime1
-                let ks2 = M.keys filesPrime2 
-
-                let whilenot :: Integer -> Integer -> StateT StdGen IO ([Hash], [Hash])
-                    whilenot oldD modulo = do
-                    p <- makePrime low high 
-                    let newP = p * modulo 
-
-                    -- Currently, we are only considering not-recursive dirs
-                    let pi1 = mkProduct p ks1
-                    let pi2 = mkProduct p ks2
-
-                    let d' = (pi1 * modularInv p pi2) `mod` p 
-                    let d = crt [(d',p), (oldD, modulo)]
-                    let (a, b) = minFraction d newP
-
-                    let newHashes = detChanges a ks1
-                        deleteHashes = detChanges b ks2
-                        okNew = (product newHashes `mod` newP) == a
-                        okDelete = (product deleteHashes `mod` newP) == b
-
-                    if (okNew && okDelete) 
-                        then return (newHashes, deleteHashes)
-                        else whilenot d newP
-                
-                (newHashes, deleteHashes) <- whilenot 0 1
-            
-                let newFiles = catMaybes $ 
-                        map (\h -> M.lookup h filesPrime1) newHashes
-                    deleteFiles = catMaybes $ 
-                        map (\h -> M.lookup h filesPrime2) deleteHashes
-                return (newFiles, deleteFiles)
-
-            print newFiles
-            print deleteFiles
-            exitSuccess
+	Main -> undefined 
 
         Oscar -> withSocketsDo $ do
             oscarg <- getStdGen
@@ -104,14 +51,10 @@ main = do
                         Nothing -> error "no hostname for neil."
                         Just neil -> neil
             (_, files2, _) <- toDir (dir t2) ""
+            filesPrime2 <- (flip evalStateT) oscarg $ 
+                mapKeysM nextShiftedPrime files2
             channel <- connectTo hostname port
-            filesPrime2 <- (flip evalStateT) oscarg $ mapKeysM nextShiftedPrime files2
             let ks2 = M.keys filesPrime2 
-            g <- case seed config of
-                    Nothing -> error "no seed sent by Main."
-                    Just s -> return $ mkStdGen s
-            let low = 2 ^ (pSize config) :: Integer
-                high = 2 ^ (pSize config + 1) :: Integer
             hSetBuffering channel LineBuffering        
             let dowhile gen = do
                 let (r,g') = randomR (low, high) gen
@@ -131,13 +74,9 @@ main = do
             (channel, _, _) <- accept socket
             nielg <- getStdGen 
             (_, files1, _) <- toDir (dir t1) ""
-            filesPrime1 <- (flip evalStateT) nielg $ mapKeysM nextShiftedPrime files1
+            filesPrime1 <- (flip evalStateT) nielg $ 
+                mapKeysM nextShiftedPrime files1
             let ks1 = M.keys filesPrime1
-            g <- case seed config of
-                    Nothing -> error "no seed sent by Main."
-                    Just s -> return $ mkStdGen s
-            let low = 2 ^ (pSize config) :: Integer
-                high = 2 ^ (pSize config + 1) :: Integer
             hSetBuffering channel LineBuffering
             let dowhile oldD oldPs gen = do
                 let (r,g') = randomR (low, high) gen
@@ -171,8 +110,3 @@ roundN oldD oldPs p pi2 ks1 =
     if okNew
         then (newPs, d, Just (b, newHashes))
         else (newPs, d, Nothing)
-
-roundO :: Integer -> [Hash] -> Integer -> [Hash]
-roundO b ks2 newPs =
-    let deleteHashes = detChanges b ks2
-    in deleteHashes
