c901f46f8c46f07011851ea5607a414e6e695ef4
Fabrice Benhamouda <fabrice_102@yahoo.fr>
2012-08-04 20:37:53 +0200

    btrsync.py works (except for permissions)

--- btrsync.py
+++ btrsync.py
@@ -8,6 +8,8 @@ import operator
 import argparse
 import sys
 import time
+import shutil
+from subprocess import Popen, PIPE, STDOUT
 
 P_SIZE    = 1024
 HASH_SIZE = 160
@@ -28,27 +30,38 @@ def send(a):
   sys.stdout.flush()
   eprint("Send " + repr(a))
 
-def hash(path,other=""):
+def hash_to_prime(h):
+  # TODO: improve this !
+  return gmpy.next_prime((h & ((1<<(HASH_SIZE-16+1)) - 1)) << 16)
+
+
+def hash_file(path,other=""):
   """ Return (h,hcontents) where:
           h is a hash of the path, the string other and the content to a prime
           hcontent is a classical hash of the content
       TODO: add permissions """
   f = file(path)
   content = f.read()
-  h = int(hashlib.sha1(path+'\0'+other+'\0'+content).hexdigest(),16)
-  hcontent = int(hashlib.sha1(content).hexdigest(),16)
+  h = int(hashlib.sha1('f\0'+path+'\0'+other+'\0'+content).hexdigest(), 16)
+  hcontent = int(hashlib.sha1(content).hexdigest(), 16)
   f.close()
-  # TODO
-  h = gmpy.next_prime((h & ((1<<(HASH_SIZE-16+1)) - 1)) << 16)
-  return (h,hcontent)
+  return (hash_to_prime(h), hcontent)
+
+def hash_dirname(path,other=""):
+  h = int(hashlib.sha1('d\0'+path+'\0'+other).hexdigest(), 16)
+  return hash_to_prime(h)
 
 def hash_dir():
   hashes = {}
   for root, dirs, files in os.walk('.'):
+    for dirname in dirs:
+      path = root+'/'+dirname
+      h = hash_dirname(path)
+      hashes[h] = (path,True,None)
     for filename in files:
       path = root+'/'+filename
-      (h,hcontent) = hash(path)
-      hashes[h] = (path,hcontent)
+      (h,hcontent) = hash_file(path)
+      hashes[h] = (path,False,hcontent)
   return hashes
 
 def generate_next_p():
@@ -116,17 +129,67 @@ def round_neil(hashes,pi_oscar,prev_pp=1,prev_d=1):
   else:
     return (pp,d,None)
 
+def sync_oscar(root_neil, root_oscar, files_neil, files_oscar):
+  """ Actually perform the synchronisation, given a list of files in Oscar and not in Neil, and a list of files in Neil and not in Oscar """
+  # TODO: can still be optimize, we should take advantage of moves
+  # TODO: remote...
+  files_by_path_oscar = {}
+  files_by_content_oscar = {} # future -> for moves
+  files_by_path_neil = {}
+
+  # Remove folders and create indexes...
+  for (path,isdir,hcontent) in files_oscar:
+    if isdir==True:
+      if os.path.isdir(path):
+        # We may have already removed the path -> the test (can be optimize by sorting the paths maybe)
+        shutil.rmtree(path)
+    else:
+      files_by_path_oscar[path] = True
+      files_by_content_oscar[hcontent] = True
+
+  # Create folders and create indexes
+  for (path,isdir,hcontent) in files_neil:
+    if isdir==True:
+      if os.path.isdir(path):
+        pass
+      elif os.path.isfile(path):
+        os.unlink(path)
+        os.makedirs(path)
+      else:
+        os.makedirs(path)
+    else:
+      files_by_path_neil[path] = True
+
+  # Remove files not in common
+  for (path,isdir,hcontent) in files_oscar:
+    if isdir==False:
+      if os.path.isfile(path):
+        if files_by_path_neil.has_key(path) == False:
+          os.unlink(path)
+
+  # Synchronize the other files
+  rsync = Popen(['rsync', '--files-from=-',root_neil,root_oscar], stdout=sys.stderr, stdin=PIPE, stderr=STDOUT)
+  for (path,isdir,hcontent) in files_neil:
+    if isdir==False:
+      rsync.stdin.write(path+"\n")
+  rsync.communicate()[0]
+  rsync.stdin.close()
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("--origin", help="", action="store_true")
   parser.add_argument("--destination", help="", action="store_true")
-  parser.add_argument("neil_dir", help="")
-  parser.add_argument("oscar_dir", help="")
+  parser.add_argument("root_neil", help="")
+  parser.add_argument("root_oscar", help="")
   args = parser.parse_args()
+
+  root_neil = os.path.abspath(args.root_neil)
+  root_oscar = os.path.abspath(args.root_oscar)
+
   if args.origin:
     # Neil
     eprint("start Neil")
-    os.chdir(args.neil_dir)
+    os.chdir(root_neil)
     hashes = hash_dir()
 
     pp = 1
@@ -140,7 +203,7 @@ def main():
   elif args.destination:
     # Oscar
     eprint("start Oscar")
-    os.chdir(args.oscar_dir)
+    os.chdir(root_oscar)
     hashes = hash_dir()
 
     res_neil = None
@@ -157,11 +220,12 @@ def main():
     eprint(files_neil)
     eprint("Oscar files:")
     eprint(files_oscar)
+    sync_oscar(root_neil,root_oscar,files_neil,files_oscar)
 
 
   else:
-    print "python btrsync.py --origin %s %s" % (args.neil_dir,args.oscar_dir)
-    print "python btrsync.py --destination %s %s" % (args.neil_dir,args.oscar_dir)
+    print "btrsync.py --origin %s %s" % (root_neil,root_oscar)
+    print "btrsync.py --destination %s %s" % (root_neil,root_oscar)
 
 
 hashes = hash_dir()
