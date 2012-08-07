7733d2895f2242aff0d1950c58e754be65901403
Antoine Amarilli <a3nm@a3nm.net>
2012-08-07 20:20:36 +0200

    Add options to be fair to rsync
    Try to account for the fact that we transfer less stuff than rsync by giving it
    the opportunity to be clever. In practice, I think everything gets transferred
    anyway, so it is a bit silly.

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # benchmark.sh FROM TO
-# run btrsync $FROM $TO and rsync --delete -Iav $FROM $TO and show the data transferred
+# run btrsync $FROM $TO and rsync --delete -Iav --chmod=* --no-* $FROM $TO and show the data transferred
 
 set -x
 
@@ -17,7 +17,9 @@ rsync -a "$TO" "$TDIR2/"
 T1=$(date +%s%N | cut -b1-13)
 BTRSYNC=$(time ./btrsync.sh "$FROM" "$TDIR/" | tail -1)
 T2=$(date +%s%N | cut -b1-13)
-RSYNC=$(time rsync --delete -Iav "$FROM" "$TDIR2/" | tail -2 | head -1)
+RSYNC=$(time rsync --delete -Iav --no-perms --chmod="a=rx,u+w" --no-links \
+  --no-hard-links --no-acls --no-xattrs --no-owner --no-group --no-devices \
+  --no-specials --no-times "$FROM" "$TDIR2/" | tail -2 | head -1)
 T3=$(date +%s%N | cut -b1-13)
 TBTRSYNC=$(($T2 - $T1))
 TRSYNC=$(($T3 - $T2))
