9af6d4e13569af39fc4eaa20ea452d6a6cf9f7a7
Antoine Amarilli <a3nm@a3nm.net>
2012-10-11 13:48:44 +0200

    change the output format of mkresults.sh

--- doc/report/mkresults.sh
+++ doc/report/mkresults.sh
@@ -1,7 +1,7 @@
 # use this to regenerate the table data
 
 cat results |
-  awk '{printf "\\texttt{%s} & \\texttt{%s} & %d & %d & %d & %d & %d & %.0f \\% & %.1f & %.1f \\\\\n",
-    $1, $2, $5, $6, $3, $4, $7, 100*(($3+$4)/($5+$6)), $9/1000, $8/1000}' |
+  awk '{printf "\\texttt{%s} & \\texttt{%s} & %d & %d & %d & %d & %d & %.1f & %.1f & %.1f \\\\\n",
+  $1, $2, $5, $6, $3, $4, $7, ($3+$4)/($5+$6), $9/1000, $8/1000}' |
     sed 's/_/\\_/g;s!test/!!g' | tr -d '/' > results.tex
 
