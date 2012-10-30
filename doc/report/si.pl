7e7061cb6e350f64a75131c58cdb1753a2c2eb64
Antoine Amarilli <a3nm@a3nm.net>
2012-10-30 23:12:51 +0100

    add results to Makefile, use SI prefixes

---
+++ doc/report/si.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# use SI prefixes in results.tex
+
+sub prefix {
+  $a = $_[0];
+  return sprintf("%.0fM", $a/(1000*1000)) if abs($a) > 1000*1000;
+  return sprintf("%.0fk", $a/(1000)) if abs($a) > 1000;
+  return $a;
+}
+
+while (<>) {
+  my @l = split;
+  for (my $i = 4; $i < 14; $i+=2) {
+    $l[$i] = prefix($l[$i]);
+  }
+  my $x = join(" ", @l);
+  print "$x\n";
+}
+
