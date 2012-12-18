1e169df93a5dbdfa8c79f964a040d856cef3ff27
Fabrice Benhamouda <fabrice_102@yahoo.fr>
2012-12-18 22:54:04 +0100

    improve figures and single.sh...

--- doc/report/single.sh
+++ doc/report/single.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-sed '/^\\bibliographystyle.*/ {
+sed -r '/^\\bibliographystyle.*/ {
   r btrsync.bbl
+  d
+}
+/\\input\{results\.tex\}/ {
+  r results.tex
   d
 }' < btrsync.tex > btrsync-single.tex
