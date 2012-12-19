df86f0e4b8a02f18d76af0d2f01606538ce8f964
Fabrice Ben Hamouda <fabrice.ben.hamouda@ens.fr>
2012-12-19 17:27:00 +0100

    better support of full/tgc versions, correction of small typos

--- doc/report/single.sh
+++ doc/report/single.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 sed -r '/^\\bibliographystyle.*/ {
-  r btrsync.bbl
+  r btrsync-tgc.bbl
   d
 }
 /\\input\{results\.tex\}/ {
   r results.tex
   d
-}' < btrsync.tex > btrsync-single.tex
+}' < btrsync.tex > btrsync-tgc-single.tex
